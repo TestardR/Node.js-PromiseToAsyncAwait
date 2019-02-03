8dc362c7771a5977226ec987a36d90a9fa0b28ce
Romain <rmtestard@gmail.com>
2019-02-03 16:17:38 +0100

    first commit

---
+++ ajax.js
@@ -0,0 +1,19 @@
+// make an ajax request ! Use the 'fetch' function.
+// https://rallycoding.herokuapp.com/
+
+// const fetch = require('node-fetch');
+
+// function fetchAlbums() {
+//   fetch('https://rallycoding.herokuapp.com/api/music_albums')
+//     .then(res => res.json())
+//     .then(json => console.log(json));
+// }
+
+async function fetchAlbums() {
+  const res = await fetch('https://rallycoding.herokuapp.com/api/music_albums');
+  const json = await res.json();
+
+  console.log(json);
+}
+
+fetchAlbums();
