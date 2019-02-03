4e52a4c42daebe67357bab3128a89f9e8396eec0
Romain <rmtestard@gmail.com>
2019-02-03 16:20:15 +0100

    ES6

--- ajax.js
+++ ajax.js
@@ -9,11 +9,11 @@
 //     .then(json => console.log(json));
 // }
 
-async function fetchAlbums() {
+const fetchAlbums = async () => {
   const res = await fetch('https://rallycoding.herokuapp.com/api/music_albums');
   const json = await res.json();
 
   console.log(json);
-}
+};
 
 fetchAlbums();
